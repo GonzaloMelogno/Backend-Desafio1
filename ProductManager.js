429f8fe4de02b81c3e8779c88ff8369b219cf805
Gonzalo Melogno <gonzalo.melogno@snig.gub.uy>
2024-01-24 11:23:17 -0300

    Version 2.0

--- ProductManager.js
+++ ProductManager.js
@@ -29,6 +29,7 @@ class ProductManager {
     productData.id = this.nextId++;
     this.products.push(productData);
     console.log('Product added successfully:', productData);
+    this.saveProductsToJSON();
   }
 
   validateProductData(productData) {
@@ -51,8 +52,47 @@ class ProductManager {
     return product;
   }
 
+
+deleteProductById(id) {
+  const productIndex = this.products.findIndex(p => p.id === id);
+
+  if (productIndex === -1) {
+    console.error('Product not found.');
+    return;
+  }
+
+  const deletedProduct = this.products.splice(productIndex, 1)[0];
+
+  this.saveProductsToJSON();
+
+  console.log('Product deleted successfully:', deletedProduct);
 }
 
+saveProductsToJSON() {
+  try {
+    const jsonData = JSON.stringify(this.products, null, 2);
+    fs.writeFileSync('./productos.json', jsonData);
+    console.log('Products saved to productos.json successfully.');
+  } catch (error) {
+    console.error('Error saving products to JSON:', error.message);
+  }
+}
+
+updateProductById (id, updatedData) {
+  const productIndex = this.products.findIndex(p => p.id === id);
+
+  if (productIndex === -1) {
+    console.error('Product not found.');
+    return;
+  }
+
+  this.products[productIndex] = { ...this.products[productIndex], ...updatedData, id };
+
+  this.saveProductsToJSON();
+
+  console.log('Product updated successfully:', this.products[productIndex]);
+}
+}
 
 
 // casos de uso
@@ -63,3 +103,17 @@ productManager.getProducts();
 const productIdToFind = 4;  
 const productById = productManager.getProductById(productIdToFind);
 console.log(`Product with ID ${productIdToFind}:`, productById);
+
+const productIdToUpdate = 3;
+const updatedData = {
+  title: 'Remera',
+  description: 'Nuestra remera de evermeet es una locura!! la mejor calidad y diseño de nuestro equipo desde 2021',
+  price: 1890,
+  thumbnail: '"../imagenes/Camiseta.png',
+  stock: 50
+};
+
+productManager.updateProductById(productIdToUpdate, updatedData);
+
+const productIdToDelete = 8;
+productManager.deleteProductById(productIdToDelete);
